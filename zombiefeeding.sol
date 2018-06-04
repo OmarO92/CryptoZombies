6397e01c7841c9d9ec75e2acf352588777c14146
Omar Oseguera <omaroseguera92@gmail.com>
2018-06-03 22:33:01 -0700

    Lesson 3
    Lesson 3 complete, updated all files.

--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -3,43 +3,43 @@ pragma solidity ^0.4.19;
 import "./zombiefactory.sol";
 
 contract KittyInterface {
-    function getKitty(uint256 _id) external view returns (
-        bool isGestating,
-        bool isReady,
-        uint256 cooldownIndex,
-        uint256 nextActionAt,
-        uint256 siringWithId,
-        uint256 birthTime,
-        uint256 matronId,
-        uint256 sireId,
-        uint256 generation,
-        uint256 genes);
+  function getKitty(uint256 _id) external view returns (
+    bool isGestating,
+    bool isReady,
+    uint256 cooldownIndex,
+    uint256 nextActionAt,
+    uint256 siringWithId,
+    uint256 birthTime,
+    uint256 matronId,
+    uint256 sireId,
+    uint256 generation,
+    uint256 genes
+  );
 }
 
 contract ZombieFeeding is ZombieFactory {
 
-    // 1. Remove this:
-    address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
-    // 2. Change this to just a declaration:
-    KittyInterface kittyContract = KittyInterface(ckAddress);
+  KittyInterface kittyContract;
 
-    // 3. Add setKittyContractAddress method here
+  function setKittyContractAddress(address _address) external onlyOwner {
+    kittyContract = KittyInterface(_address);
+  }
 
-    function feedAndMultiply(uint _zombieId, uint _targetDna, string _species) public {
-        require(msg.sender == zombieToOwner[_zombieId]);
-        Zombie storage myZombie = zombies[_zombieId];
-        _targetDna = _targetDna % dnaModulus;
-        uint newDna = (myZombie.dna + _targetDna) / 2;
-        if (keccak256(_species) == keccak256("kitty")) {
-            newDna = newDna - newDna % 100 + 99;
-        }
-        _createZombie("NoName", newDna);
+  function feedAndMultiply(uint _zombieId, uint _targetDna, string _species) public {
+    require(msg.sender == zombieToOwner[_zombieId]);
+    Zombie storage myZombie = zombies[_zombieId];
+    _targetDna = _targetDna % dnaModulus;
+    uint newDna = (myZombie.dna + _targetDna) / 2;
+    if (keccak256(_species) == keccak256("kitty")) {
+      newDna = newDna - newDna % 100 + 99;
     }
+    _createZombie("NoName", newDna);
+  }
 
-    function feedOnKitty(uint _zombieId, uint _kittyId) public {
-        uint kittyDna;
-        (,,,,,,,,,kittyDna) = kittyContract.getKitty(_kittyId);
-        feedAndMultiply(_zombieId, kittyDna, "kitty");
-    }
+  function feedOnKitty(uint _zombieId, uint _kittyId) public {
+    uint kittyDna;
+    (,,,,,,,,,kittyDna) = kittyContract.getKitty(_kittyId);
+    feedAndMultiply(_zombieId, kittyDna, "kitty");
+  }
 
 }
